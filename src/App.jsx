62968d2ce7fb1fa3e9df33608b8b65955bdb0549
Moawad Ibrahim <mo3wadibrahim@gmail.com>
2026-07-14 17:21:56 +0300

    App Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,53 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/Home.jsx";
 import About from "./components/About.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
 function App() {
   return (
-    <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <hr />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <p>
+        <a href="http://localhost:5173/">Home</a>
+      </p>
+      <a href="http://localhost:5173/about">About</a>
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
 export default App;
+
+// // // // // // // // // ** BrowserRouter ** // // // // // // // // //
+// import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+// import Home from "./components/Home.jsx";
+// import About from "./components/About.jsx";
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <nav>
+//         <ul>
+//           <li>
+//             <Link to="/">Home</Link>
+//           </li>
+//           <li>
+//             <Link to="/about">About</Link>
+//           </li>
+//         </ul>
+//       </nav>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
