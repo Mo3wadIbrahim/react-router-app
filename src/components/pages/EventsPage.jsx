f8d314b953f75324d828f4d990e53ce7cd0ea5db
Moawad Ibrahim <mo3wadibrahim@gmail.com>
2026-07-29 01:03:59 +0300

    App Update

--- src/components/pages/EventsPage.jsx
+++ src/components/pages/EventsPage.jsx
@@ -1,37 +1,9 @@
-import EventItem from "../EventItem";
+import { useLoaderData } from "react-router-dom";
+import EventsList from "../EventsList.jsx";
 
-const DUMMY_EVENTS = [
-  {
-    id: "e1",
-    image: "some alt text",
-    title: "Event Title",
-    date: "12 May 2026",
-    description: "some Information about event",
-  },
-  {
-    id: "e2",
-    image: "some alt text",
-    title: "Event Title",
-    date: "12 May 2026",
-    description: "some Information about event",
-  },
-  {
-    id: "e3",
-    image: "some alt text",
-    title: "Event Title",
-    date: "12 May 2026",
-    description: "some Information about event",
-  },
-];
-export default function EventsPage() {
-  return (
-    <>
-      <h1>Events Page</h1>;
-      <ul>
-        {DUMMY_EVENTS.map((event) => (
-          <EventItem event={event} key={event.id} />
-        ))}
-      </ul>
-    </>
-  );
+function EventsPage() {
+  const events = useLoaderData();
+  return <EventsList events={events} />;
 }
+
+export default EventsPage;
